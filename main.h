6a8f538ab401a5dc4e2b3ee9663435333048ec77
amr89mohamed <amr89mohamed@gmail.com>
2024-01-30 18:45:52 -0800

    4th v

--- main.h
+++ main.h
@@ -14,16 +14,13 @@ int print_octal(va_list o);
 int print_x(va_list x);
 int print_X(va_list X);
 int print_rot13(va_list R);
-/**
+/*
   * struct code_format - Struct format
-  *
   * @sc: The specifiers
   * @f: The function associated
-  */
-typedef struct code_format
+  */typedef struct code_format
 {
-	char *sc;
-	int (*f)(va_list);
-}to_find;
-
-#endif /* HOLBERTONH */
+char *sc;
+int (*f)(va_list);
+} code_f;
+#endif 
